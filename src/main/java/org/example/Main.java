eab867a8652eefa5ef8315200ae264350abfc82c
Lucas Lima <lukinha-lenta@hotmail.com>
2023-10-30 17:40:40 -0300

    Selecionando rows vazias

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import org.apache.poi.ss.usermodel.*;
 
 import javax.swing.*;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -20,8 +20,7 @@ public class Main {
         if (result == JFileChooser.APPROVE_OPTION) {
             filePath = fileChooser.getSelectedFile().getAbsolutePath();
             JOptionPane.showMessageDialog(null, "Arquivo Selecionado" + filePath);
-            FileInputStream fileInput = new FileInputStream(filePath);
-            Workbook workbook = WorkbookFactory.create(fileInput);
+            Workbook workbook = WorkbookFactory.create(new File(filePath));
             Sheet sheet = workbook.getSheetAt(0);
             DataFormatter dataFormatter = new DataFormatter();
             //Acessar o banco
@@ -36,31 +35,45 @@ public class Main {
             int rowIndex;
             for (rowIndex = 2; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
                 Row row = sheet.getRow(rowIndex);
-                String id = dataFormatter.formatCellValue(row.getCell(0));
-                String ncm = dataFormatter.formatCellValue(row.getCell(9));
-                String cfop = dataFormatter.formatCellValue(row.getCell(10));
-                String cest = dataFormatter.formatCellValue(row.getCell(11));
-                String cst = dataFormatter.formatCellValue(row.getCell(12));
-                String icms = dataFormatter.formatCellValue(row.getCell(13));
-                String pisCod = dataFormatter.formatCellValue(row.getCell(14));
-                String pisA = dataFormatter.formatCellValue(row.getCell(15));
-                String cofinsCod = dataFormatter.formatCellValue(row.getCell(16));
-                String cofinsA = dataFormatter.formatCellValue(row.getCell(17));
-
-                PreparedStatement preparedStatement = connection.prepareStatement("UPDATE product " +
-                        "SET ncm = " + ncm
-                        + ", cfop = " + cfop
-                        + ", tax4_code = " + cest
-                        + ", tax1_code = " + cst
-                        + ", tax1 = " + icms
-                        + ", tax2_code = " + pisCod
-                        + ", tax2 = " + pisA
-                        + ", tax3_code = " + cofinsCod
-                        + ", tax3 = " + cofinsA
-                        + " WHERE internal_code = " + id);
-                preparedStatement.execute();
-                System.out.println(preparedStatement);
+                if (isRowEmpty(row)) {
+                    break;
+                } else {
+                    String id = dataFormatter.formatCellValue(row.getCell(2));
+                    String ncm = dataFormatter.formatCellValue(row.getCell(3));
+                    String cfop = dataFormatter.formatCellValue(row.getCell(4));
+                    String cest = dataFormatter.formatCellValue(row.getCell(12));
+                    String cst = dataFormatter.formatCellValue(row.getCell(9));
+                    String icms = dataFormatter.formatCellValue(row.getCell(5));
+                    String pisCod = dataFormatter.formatCellValue(row.getCell(10));
+                    String pisA = dataFormatter.formatCellValue(row.getCell(6));
+                    String cofinsCod = dataFormatter.formatCellValue(row.getCell(11));
+                    String cofinsA = dataFormatter.formatCellValue(row.getCell(7));
+                    PreparedStatement preparedStatement = connection.prepareStatement("UPDATE product " +
+                            "SET ncm = " + ncm
+                            + ", cfop = " + cfop
+                            + ", tax4_code = " + cest
+                            + ", tax1_code = " + cst
+                            + ", tax1 = " + icms
+                            + ", tax2_code = " + pisCod
+                            + ", tax2 = " + pisA
+                            + ", tax3_code = " + cofinsCod
+                            + ", tax3 = " + cofinsA
+                            + " WHERE internal_code = " + id);
+                    preparedStatement.execute();
+                    System.out.println(preparedStatement);
+                }
             }
         }
+    }
+    public static boolean isRowEmpty(Row row) {
+        if (row != null) {
+            for (int cellIndex = 0; cellIndex < row.getLastCellNum(); cellIndex++) {
+                Cell cell = row.getCell(cellIndex);
+                if (cell != null && cell.getCellType() != CellType.BLANK) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
